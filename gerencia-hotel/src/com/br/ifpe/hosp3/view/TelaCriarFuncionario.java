a29e36f3c0fc37f6dfc88a4fe0fd28b9b00a03a6
MabiGermano <mabi.germano.e@gmail.com>
2019-12-09 22:35:16 -0300

    correção para layout com rolagem

--- gerencia-hotel/src/com/br/ifpe/hosp3/view/TelaCriarFuncionario.java
+++ gerencia-hotel/src/com/br/ifpe/hosp3/view/TelaCriarFuncionario.java
@@ -10,22 +10,26 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import com.br.ifpe.hosp3.model.Funcionario;
+import javax.swing.JScrollBar;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class TelaCriarFuncionario extends JInternalFrame {
-	private JTextField txtNomeFunc;
-	private JTextField txtCpfFunc;
-	private JTextField txtEmailFunc;
-	private JTextField txtPassFunc;
-	private JTextField txtCodFunc;
-	private JTextField txtTelFunc;
-	private JTextField txtCepFunc;
-	private JTextField txtRuaFunc;
-	private JTextField txtNumFunc;
-	private JTextField txtBairroFunc;
-	private JTextField txtCompFunc;
-	private JTextField txtCidadeFunc;
-	private JTextField txtEstadoFunc;
-	private JTextField txtPaisFunc;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
 
 	public TelaCriarFuncionario(Funcionario funcionario) {
 		this();
@@ -41,146 +45,257 @@ public class TelaCriarFuncionario extends JInternalFrame {
 		
 		JPanel panel = new JPanel();
 		scrollPane.setViewportView(panel);
-		panel.setLayout(null);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
 		
-		JLabel lblCadastrarFuncionrio = new JLabel("Cadastrar Funcionário");
-		lblCadastrarFuncionrio.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblCadastrarFuncionrio.setBounds(182, 27, 144, 14);
-		panel.add(lblCadastrarFuncionrio);
+		JLabel label = new JLabel("Cadastrar Funcionário");
+		label.setFont(new Font("Dialog", Font.PLAIN, 14));
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 0;
+		panel.add(label, gbc_label);
 		
-		JLabel lblNomeFunc = new JLabel("Nome");
-		lblNomeFunc.setBounds(40, 65, 46, 14);
-		panel.add(lblNomeFunc);
+		JLabel label_1 = new JLabel("Nome");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.insets = new Insets(0, 0, 5, 0);
+		gbc_label_1.gridx = 0;
+		gbc_label_1.gridy = 1;
+		panel.add(label_1, gbc_label_1);
 		
-		txtNomeFunc = new JTextField();
-		txtNomeFunc.setBounds(39, 81, 188, 20);
-		panel.add(txtNomeFunc);
-		txtNomeFunc.setColumns(10);
+		textField = new JTextField();
+		textField.setColumns(10);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 2;
+		panel.add(textField, gbc_textField);
 		
-		JLabel lblCpfFunc = new JLabel("CPF");
-		lblCpfFunc.setBounds(267, 65, 46, 14);
-		panel.add(lblCpfFunc);
+		JLabel label_2 = new JLabel("CPF");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.insets = new Insets(0, 0, 5, 0);
+		gbc_label_2.gridx = 0;
+		gbc_label_2.gridy = 3;
+		panel.add(label_2, gbc_label_2);
 		
-		txtCpfFunc = new JTextField();
-		txtCpfFunc.setBounds(267, 81, 188, 20);
-		panel.add(txtCpfFunc);
-		txtCpfFunc.setColumns(10);
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 0;
+		gbc_textField_1.gridy = 4;
+		panel.add(textField_1, gbc_textField_1);
 		
-		JLabel lblEmailFunc = new JLabel("E-mail");
-		lblEmailFunc.setBounds(40, 127, 46, 14);
-		panel.add(lblEmailFunc);
+		JLabel label_3 = new JLabel("E-mail");
+		GridBagConstraints gbc_label_3 = new GridBagConstraints();
+		gbc_label_3.insets = new Insets(0, 0, 5, 0);
+		gbc_label_3.gridx = 0;
+		gbc_label_3.gridy = 5;
+		panel.add(label_3, gbc_label_3);
 		
-		txtEmailFunc = new JTextField();
-		txtEmailFunc.setBounds(40, 144, 187, 20);
-		panel.add(txtEmailFunc);
-		txtEmailFunc.setColumns(10);
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 0;
+		gbc_textField_2.gridy = 6;
+		panel.add(textField_2, gbc_textField_2);
 		
-		JLabel lblPassFunc = new JLabel("Palavra passe");
-		lblPassFunc.setBounds(267, 127, 75, 14);
-		panel.add(lblPassFunc);
+		JLabel label_4 = new JLabel("Palavra passe");
+		GridBagConstraints gbc_label_4 = new GridBagConstraints();
+		gbc_label_4.insets = new Insets(0, 0, 5, 0);
+		gbc_label_4.gridx = 0;
+		gbc_label_4.gridy = 7;
+		panel.add(label_4, gbc_label_4);
 		
-		txtPassFunc = new JTextField();
-		txtPassFunc.setBounds(267, 144, 188, 20);
-		panel.add(txtPassFunc);
-		txtPassFunc.setColumns(10);
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 0;
+		gbc_textField_3.gridy = 8;
+		panel.add(textField_3, gbc_textField_3);
 		
-		JLabel lblCodFunc = new JLabel("Código");
-		lblCodFunc.setBounds(40, 190, 46, 14);
-		panel.add(lblCodFunc);
+		JLabel label_5 = new JLabel("Código");
+		GridBagConstraints gbc_label_5 = new GridBagConstraints();
+		gbc_label_5.insets = new Insets(0, 0, 5, 0);
+		gbc_label_5.gridx = 0;
+		gbc_label_5.gridy = 9;
+		panel.add(label_5, gbc_label_5);
 		
-		txtCodFunc = new JTextField();
-		txtCodFunc.setBounds(40, 206, 66, 20);
-		panel.add(txtCodFunc);
-		txtCodFunc.setColumns(10);
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
+		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_4.gridx = 0;
+		gbc_textField_4.gridy = 10;
+		panel.add(textField_4, gbc_textField_4);
 		
-		JLabel lblTelFunc = new JLabel("Telefone");
-		lblTelFunc.setBounds(128, 190, 46, 14);
-		panel.add(lblTelFunc);
+		JLabel label_6 = new JLabel("Telefone");
+		GridBagConstraints gbc_label_6 = new GridBagConstraints();
+		gbc_label_6.insets = new Insets(0, 0, 5, 0);
+		gbc_label_6.gridx = 0;
+		gbc_label_6.gridy = 11;
+		panel.add(label_6, gbc_label_6);
 		
-		txtTelFunc = new JTextField();
-		txtTelFunc.setBounds(128, 206, 144, 20);
-		panel.add(txtTelFunc);
-		txtTelFunc.setColumns(10);
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
+		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_5.gridx = 0;
+		gbc_textField_5.gridy = 12;
+		panel.add(textField_5, gbc_textField_5);
 		
-		JLabel lblCepFunc = new JLabel("CEP");
-		lblCepFunc.setBounds(296, 190, 46, 14);
-		panel.add(lblCepFunc);
+		JLabel label_7 = new JLabel("CEP");
+		GridBagConstraints gbc_label_7 = new GridBagConstraints();
+		gbc_label_7.insets = new Insets(0, 0, 5, 0);
+		gbc_label_7.gridx = 0;
+		gbc_label_7.gridy = 13;
+		panel.add(label_7, gbc_label_7);
 		
-		txtCepFunc = new JTextField();
-		txtCepFunc.setBounds(296, 206, 159, 20);
-		panel.add(txtCepFunc);
-		txtCepFunc.setColumns(10);
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
+		gbc_textField_6.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_6.gridx = 0;
+		gbc_textField_6.gridy = 14;
+		panel.add(textField_6, gbc_textField_6);
 		
-		JLabel lblRuaFunc = new JLabel("Rua");
-		lblRuaFunc.setBounds(40, 249, 46, 14);
-		panel.add(lblRuaFunc);
+		JLabel label_8 = new JLabel("Rua");
+		GridBagConstraints gbc_label_8 = new GridBagConstraints();
+		gbc_label_8.insets = new Insets(0, 0, 5, 0);
+		gbc_label_8.gridx = 0;
+		gbc_label_8.gridy = 15;
+		panel.add(label_8, gbc_label_8);
 		
-		txtRuaFunc = new JTextField();
-		txtRuaFunc.setBounds(40, 265, 232, 20);
-		panel.add(txtRuaFunc);
-		txtRuaFunc.setColumns(10);
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
+		gbc_textField_7.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_7.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_7.gridx = 0;
+		gbc_textField_7.gridy = 16;
+		panel.add(textField_7, gbc_textField_7);
 		
-		JLabel lblNumFunc = new JLabel("Número");
-		lblNumFunc.setBounds(293, 249, 46, 14);
-		panel.add(lblNumFunc);
+		JLabel label_9 = new JLabel("Número");
+		GridBagConstraints gbc_label_9 = new GridBagConstraints();
+		gbc_label_9.insets = new Insets(0, 0, 5, 0);
+		gbc_label_9.gridx = 0;
+		gbc_label_9.gridy = 17;
+		panel.add(label_9, gbc_label_9);
 		
-		txtNumFunc = new JTextField();
-		txtNumFunc.setBounds(291, 265, 51, 20);
-		panel.add(txtNumFunc);
-		txtNumFunc.setColumns(10);
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		GridBagConstraints gbc_textField_8 = new GridBagConstraints();
+		gbc_textField_8.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_8.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_8.gridx = 0;
+		gbc_textField_8.gridy = 18;
+		panel.add(textField_8, gbc_textField_8);
 		
-		JLabel lblBairroFunc = new JLabel("Bairro");
-		lblBairroFunc.setBounds(355, 249, 46, 14);
-		panel.add(lblBairroFunc);
+		JLabel label_10 = new JLabel("Bairro");
+		GridBagConstraints gbc_label_10 = new GridBagConstraints();
+		gbc_label_10.insets = new Insets(0, 0, 5, 0);
+		gbc_label_10.gridx = 0;
+		gbc_label_10.gridy = 19;
+		panel.add(label_10, gbc_label_10);
 		
-		txtBairroFunc = new JTextField();
-		txtBairroFunc.setBounds(355, 265, 100, 20);
-		panel.add(txtBairroFunc);
-		txtBairroFunc.setColumns(10);
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		GridBagConstraints gbc_textField_9 = new GridBagConstraints();
+		gbc_textField_9.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_9.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_9.gridx = 0;
+		gbc_textField_9.gridy = 20;
+		panel.add(textField_9, gbc_textField_9);
 		
-		JLabel lblCompFunc = new JLabel("Complemento");
-		lblCompFunc.setBounds(40, 306, 66, 14);
-		panel.add(lblCompFunc);
+		JLabel label_11 = new JLabel("Complemento");
+		GridBagConstraints gbc_label_11 = new GridBagConstraints();
+		gbc_label_11.insets = new Insets(0, 0, 5, 0);
+		gbc_label_11.gridx = 0;
+		gbc_label_11.gridy = 21;
+		panel.add(label_11, gbc_label_11);
 		
-		txtCompFunc = new JTextField();
-		txtCompFunc.setBounds(38, 322, 234, 20);
-		panel.add(txtCompFunc);
-		txtCompFunc.setColumns(10);
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		GridBagConstraints gbc_textField_10 = new GridBagConstraints();
+		gbc_textField_10.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_10.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_10.gridx = 0;
+		gbc_textField_10.gridy = 22;
+		panel.add(textField_10, gbc_textField_10);
 		
-		JLabel lblCidadeFunc = new JLabel("Cidade");
-		lblCidadeFunc.setBounds(293, 306, 46, 14);
-		panel.add(lblCidadeFunc);
+		JLabel label_12 = new JLabel("Cidade");
+		GridBagConstraints gbc_label_12 = new GridBagConstraints();
+		gbc_label_12.insets = new Insets(0, 0, 5, 0);
+		gbc_label_12.gridx = 0;
+		gbc_label_12.gridy = 23;
+		panel.add(label_12, gbc_label_12);
 		
-		txtCidadeFunc = new JTextField();
-		txtCidadeFunc.setBounds(293, 322, 162, 20);
-		panel.add(txtCidadeFunc);
-		txtCidadeFunc.setColumns(10);
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		GridBagConstraints gbc_textField_11 = new GridBagConstraints();
+		gbc_textField_11.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_11.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_11.gridx = 0;
+		gbc_textField_11.gridy = 24;
+		panel.add(textField_11, gbc_textField_11);
 		
-		JLabel lblEstadoFunc = new JLabel("Estado");
-		lblEstadoFunc.setBounds(40, 364, 46, 14);
-		panel.add(lblEstadoFunc);
+		JLabel label_13 = new JLabel("Estado");
+		GridBagConstraints gbc_label_13 = new GridBagConstraints();
+		gbc_label_13.insets = new Insets(0, 0, 5, 0);
+		gbc_label_13.gridx = 0;
+		gbc_label_13.gridy = 25;
+		panel.add(label_13, gbc_label_13);
 		
-		txtEstadoFunc = new JTextField();
-		txtEstadoFunc.setBounds(38, 379, 86, 20);
-		panel.add(txtEstadoFunc);
-		txtEstadoFunc.setColumns(10);
+		textField_12 = new JTextField();
+		textField_12.setColumns(10);
+		GridBagConstraints gbc_textField_12 = new GridBagConstraints();
+		gbc_textField_12.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_12.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_12.gridx = 0;
+		gbc_textField_12.gridy = 26;
+		panel.add(textField_12, gbc_textField_12);
 		
-		JLabel lblPaisFunc = new JLabel("País");
-		lblPaisFunc.setBounds(155, 364, 46, 14);
-		panel.add(lblPaisFunc);
+		JLabel label_14 = new JLabel("País");
+		GridBagConstraints gbc_label_14 = new GridBagConstraints();
+		gbc_label_14.insets = new Insets(0, 0, 5, 0);
+		gbc_label_14.gridx = 0;
+		gbc_label_14.gridy = 27;
+		panel.add(label_14, gbc_label_14);
 		
-		txtPaisFunc = new JTextField();
-		txtPaisFunc.setBounds(155, 379, 114, 20);
-		panel.add(txtPaisFunc);
-		txtPaisFunc.setColumns(10);
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		GridBagConstraints gbc_textField_13 = new GridBagConstraints();
+		gbc_textField_13.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_13.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_13.gridx = 0;
+		gbc_textField_13.gridy = 28;
+		panel.add(textField_13, gbc_textField_13);
 		
-		JButton btnCancelarFunc = new JButton("Cancelar");
-		btnCancelarFunc.setBounds(267, 420, 89, 36);
-		panel.add(btnCancelarFunc);
+		JButton button = new JButton("Cancelar");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.insets = new Insets(0, 0, 5, 0);
+		gbc_button.gridx = 0;
+		gbc_button.gridy = 29;
+		panel.add(button, gbc_button);
 		
-		JButton btnAdicionarFunc = new JButton("Adicionar");
-		btnAdicionarFunc.setBounds(366, 420, 89, 36);
-		panel.add(btnAdicionarFunc);
+		JButton button_1 = new JButton("Adicionar");
+		GridBagConstraints gbc_button_1 = new GridBagConstraints();
+		gbc_button_1.gridx = 0;
+		gbc_button_1.gridy = 30;
+		panel.add(button_1, gbc_button_1);
 
 	}
 }
